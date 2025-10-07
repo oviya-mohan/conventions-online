e1e608c2ae62df785f3d6b775455e9408ccb86ce
Biro Lab <omohan@ur.rochester.edu>
2025-10-07 13:26:29 -0400

    set rounds to 2 for demo

--- I_OT_O/__init__.py
+++ I_OT_O/__init__.py
@@ -16,7 +16,7 @@ class Constants(BaseConstants):
     players_per_group = 2
     # num_rounds = 294
     # for test runthroughs
-    num_rounds = 5
+    num_rounds = 21
 
 ################################################################################
 #CREATING SESSIONS
